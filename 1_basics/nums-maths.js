8d8de8e4aa3a02f3cc9be754a60016fd7e1e50b5
shagun57 <105437856+shagun57@users.noreply.github.com>
2024-01-31 14:37:58 +0000

    numbers and maths

---
+++ 1_basics/nums-maths.js
@@ -0,0 +1,22 @@
+//const score = 400;
+ //console.log(score);
+
+ //const balance = new Number(300);  // it will always be a number
+ //console.log(balance);
+
+ //console.log(balance.toFixed(2));
+
+ //const newNum = 121.564;
+ //console.log(newNum.toPrecision(4));
+
+ //const newNum1 = 1000000;
+ //console.log(newNum1.toLocaleString('en-IN'));  //add commas according to indian system
+
+
+ //------------------maths----------------------
+
+console.log(Math);
+//console.log(Math.abs(-5));
+
+
+console.log(Math.floor(Math.random()*10) +1);
